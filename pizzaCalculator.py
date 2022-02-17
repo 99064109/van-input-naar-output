c87c6146c1cd90fda56f5e63f671c2686e270dc5
99064109 <94683652+99064109@users.noreply.github.com>
2022-02-17 10:13:33 +0100

    Pizzacalculator

--- pizzaCalculator.py
+++ pizzaCalculator.py
@@ -27,37 +27,6 @@ print(totaalbedrag)
 
 
 
-
-
-
-
-
-
-# # bestellen = input('Wat voor pizza wilt u bestellen? Medium, Large of Extra large? '))
-# # hoeveel_medium_pizzas = int(input("Hoeveel medium pizza's wilt u bestellen? "))
-# # prijs_medium_pizzas =(float(4.95))
-# # hoeveel_large_pizzas = int(input("Hoeveel large pizza's wilt u? "))
-# # prijs_large_pizza =(float(8.95))
-# # hoeveel_extra_large_pizzas = int(input("Hoeveel extra large pizza's wilt u? "))
-# # prijs_extra_large_pizza = (float(10.95))
-
-
-# # # Ik ga nu uitrekenen wat de pizza's in totaal kosten.
-# # print(hoeveel_medium_pizzas * prijs_medium_pizzas)
-# # print(hoeveel_large_pizzas * prijs_large_pizza)
-# # print(hoeveel_extra_large_pizzas * prijs_extra_large_pizza)
-
-
-# # prijs_medium_pizzas =(float(4.95))
-# # prijs_large_pizza =(float(8.95))
-# # prijs_extra_large_pizza = (float(10.95))
-
-# print
-
-
-# Chyvayno Felicia
-# Opdracht: pizzaCalculator.py
-
 # medium pizza 25cm $4,95
 # large pizza 30cm $8,95
 # extra large pizza $35cm 10,95
